2e155f6b4bd0a976ac97546e8803b75c7f18a5aa
mjfaye <mjfaye@sina.com>
2018-07-02 09:52:48 +0800

    aa

---
+++ src/main/java/com/y2j/test/testapi/entity/Student.java
@@ -0,0 +1,55 @@
+package com.y2j.test.testapi.entity;
+
+/**
+ * @Author liuhao
+ * @Date 2018/7/2 9:48
+ */
+
+public class Student
+{
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Integer getAge()
+    {
+        return age;
+    }
+
+    public void setAge(Integer age)
+    {
+        this.age = age;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
